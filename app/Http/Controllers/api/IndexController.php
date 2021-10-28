f30d616aef7f87d77351843e82521e0765a158e0
Rowena <rowena@u-ark.com>
2021-10-28 19:02:09 +0800

    api test

--- app/Http/Controllers/api/IndexController.php
+++ app/Http/Controllers/api/IndexController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Services\UniversalService;
 use Illuminate\Http\Request;
+use App\Services\WebContentsService;
 
 class IndexController extends Controller
 {
+
+    private $webContentsService;
+
+    public function __construct(WebContentsService $webContentsService, UniversalService $universalService)
+    {
+        $this->webContentsService = $webContentsService;
+        $this->universalService = $universalService;
+    }
+
     public function index()
     {
-        return response()->json(['status'=>true,'reuslt'=>'Text OK']);
+        $level1 = $this->universalService->getFooterCategory('FOOTER_CATEGORY');
+        $data = [];
+        $items = [];
+        foreach ($level1 as $code => $name) {
+            $input['code'] = $code;
+            $level2 = $this->webContentsService->getFooter($input, 'FOOTER');
+            foreach ($level2 as $k => $v) {
+                $items[$v['parent_code']][$k]['content_name'] = $v['content_name'];
+                $items[$v['parent_code']][$k]['content_target'] = $v['content_target'];
+                $items[$v['parent_code']][$k]['content_url'] = $v['content_url'];
+            }
+
+            $data[] = array(
+                "fieldId" => $code, //ABOUT_US
+                "fieldValue" => $name, //關於我們
+                "fieldItems" => $items[$v['parent_code']]
+            );
+        }
+        return response()->json(['status' => true, 'reuslt' => $data]);
     }
 }
