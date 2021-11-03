a8c1f90f70139849d214bec061b2126fe4abafe5
Rowena <rowena@u-ark.com>
2021-11-03 09:23:31 +0800

    area api

--- app/Http/Controllers/api/DradviceController.php
+++ app/Http/Controllers/api/DradviceController.php
@@ -12,17 +12,23 @@ class DradviceController extends Controller
      */
     public function area()
     {
-        $area_api = 'http://18.178.42.56:8020/area.json';
-        //  Initiate curl
-        $ch = curl_init();
-        // Will return the response, if false it print the response
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // Set the url
-        curl_setopt($ch, CURLOPT_URL, $area_api);
-        // Execute
-        $result = curl_exec($ch);
-        // Closing
-        curl_close($ch);
-        return $result;
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://18.178.42.56:8020/area.json',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
     }
 }
