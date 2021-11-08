0414edd4abde2b04b97f595e44f7dff0f21bf12e
LEO-NB\uark <mopackp47557@gmail.com>
2021-11-08 16:03:27 +0800

    新增分類

--- resources/views/Backend/WebCategoryHierarchy/index.blade.php
+++ resources/views/Backend/WebCategoryHierarchy/index.blade.php
@@ -79,15 +79,18 @@
                                 <div class="row">
                                     <div class="row">
                                         <div class="col-sm-12">
-                                            <h4> 【<span class="title_color">@{{ category_level_2_title }}</span>】的中分類</h4>
+                                            <h4> 【<span class="title_color">@{{ category_level_2_title }}</span>】的中分類
+                                            </h4>
                                         </div>
                                     </div>
                                     <div class="col-sm-3">
-                                        <a class="btn btn-block btn-warning btn-sm" data-toggle="modal"
-                                            data-target="#addCategory" @click="CategoryModelShow('2','add')">新增中類</a>
+                                        <button class="btn btn-block btn-warning btn-sm" data-toggle="modal"
+                                            data-target="#addCategory" @click="CategoryModelShow('2','add')"
+                                            :disabled="disabled.disabled_level_2 == 1">新增中類</button>
                                     </div>
                                     <div class="col-sm-3">
-                                        <a class="btn btn-block btn-success btn-sm" @click="">儲存</a>
+                                        <a class="btn btn-block btn-success btn-sm" @click=""
+                                            :disabled="disabled.disabled_level_2 == 1">儲存</a>
                                     </div>
                                 </div>
                                 <hr>
@@ -111,8 +114,8 @@
                                                             @click="GetCategory(level_2_obj,'2')">展小類</button>
                                                     </div>
                                                     <div class="col-sm-3">
-                                                        <button type="button" class="btn btn-warning"
-                                                            data-toggle="modal" data-target="#addCategory"
+                                                        <button type="button" class="btn btn-warning" data-toggle="modal"
+                                                            data-target="#addCategory"
                                                             @click="CategoryModelShow('2','edit',level_2_obj)">編輯</button>
                                                     </div>
                                                     <div class="col-sm-3">
@@ -129,7 +132,8 @@
                                 <div class="row">
                                     <div class="row">
                                         <div class="col-sm-12">
-                                            <h4> 【<span class="title_color">@{{ category_level_3_title }}</span>】的小分類</h4>
+                                            <h4> 【<span class="title_color">@{{ category_level_3_title }}</span>】的小分類
+                                            </h4>
                                             <div v-if="category_level_3_title">
                                             </div>
                                             <div v-else>
@@ -138,10 +142,12 @@
                                     </div>
                                     <div class="col-sm-3">
                                         <a class="btn btn-block btn-warning btn-sm" data-toggle="modal"
-                                            data-target="#addCategory" @click="CategoryModelShow('3','add')">新增小類</a>
+                                            data-target="#addCategory" @click="CategoryModelShow('3','add')"
+                                            :disabled="disabled.disabled_level_3 == 1">新增小類</a>
                                     </div>
                                     <div class="col-sm-3">
-                                        <a class="btn btn-block btn-success btn-sm">儲存</a>
+                                        <a class="btn btn-block btn-success btn-sm"
+                                            :disabled="disabled.disabled_level_3 == 1">儲存</a>
                                     </div>
                                 </div>
                                 <hr>
@@ -204,8 +210,12 @@
                         category_level: '',
                         parent_id: '',
                         category_name: '',
-                        old_category_name : '' ,
-                        act :''
+                        old_category_name: '',
+                        act: ''
+                    },
+                    disabled: {
+                        disabled_level_2: 1,
+                        disabled_level_3: 1,
                     },
                     //
                     msg: {
@@ -247,19 +257,20 @@
                 },
                 CategoryModelShow(level, act, obj) {
                     // empty data
-                    this.msg.receiver_name = ''; 
+                    this.msg.receiver_name = '';
                     this.addCategory.category_name = '';
-                    this.addCategory.id = '' ; 
+                    this.addCategory.id = '';
                     this.addCategory.category_level = level;
                     this.addCategory.act = act;
-                    if(act == 'edit'){
-                        this.addCategory.old_category_name = obj.category_name ; 
-                        this.addCategory.id = obj.id ; 
+                    if (act == 'edit') {
+                        this.addCategory.old_category_name = obj.category_name;
+                        this.addCategory.id = obj.id;
                     }
                     switch (level) {
                         case '1':
                             this.addCategory.category_level = level;
                             this.addCategory.show_title = '大分類';
+                            this.addCategory.parent_id = null;
                             break;
                         case '2':
                             this.addCategory.category_level = level;
@@ -292,7 +303,7 @@
                         const response = await axios.post('/backend/web_category_hierarchy/ajax', {
                             _token: $('meta[name="csrf-token"]').attr('content'),
                             type: type,
-                            id:this.addCategory.id,
+                            id: this.addCategory.id,
                             category_level: this.addCategory.category_level,
                             parent_id: this.addCategory.parent_id,
                             category_name: this.addCategory.category_name,
@@ -300,11 +311,18 @@
                         switch (this.addCategory.category_level) {
                             case '1':
                                 this.category_level_1 = response.data.result;
+                                if (this.addCategory.act == 'edit') {
+                                    this.category_level_2 = {};
+                                    this.category_level_2_title = '';
+                                    this.category_level_3 = {};
+                                    this.category_level_3_title = '';
+                                }
                                 break;
                             case '2':
                                 this.category_level_2 = response.data.result;
-                                if(this.addCategory.act =='edit'){
-                                    this.category_level_2 
+                                if (this.addCategory.act == 'edit') {
+                                    this.category_level_3 = {};
+                                    this.category_level_3_title = '';
                                 }
                                 break;
                             case '3':
@@ -327,9 +345,24 @@
                 //     return false;
                 // }
             },
-            computed: {
-
+            watch: {
+                //監聽是否要將新增儲存開放點擊
+                category_level_2_title: function() {
+                    if (this.category_level_2_title !== '') {
+                        return this.disabled.disabled_level_2 = 0;
+                    } else {
+                        return this.disabled.disabled_level_2 = 1;
+                    }
+                },
+                category_level_3_title: function() {
+                    if (this.category_level_3_title !== '') {
+                        return this.disabled.disabled_level_3 = 0;
+                    } else {
+                        return this.disabled.disabled_level_3 = 1;
+                    }
+                },
             },
+            computed: {},
         });
 
         new requisitions().$mount('#web_category_hierarchy');
