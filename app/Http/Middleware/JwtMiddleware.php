4715d3e6b6b71f1798dbf448a046866e91b50ff6
Rowena <rowena@u-ark.com>
2021-11-11 11:00:38 +0800

    調整API回傳文字

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -7,13 +7,14 @@ use Closure;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Log;
+
 class JwtMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
@@ -21,20 +22,26 @@ class JwtMiddleware
         try {
             if ($request->bearerToken()) {
                 $token = $request->bearerToken();
+                $member = Members::where('api_token', '=', $token)->get()->toArray();
+                if (sizeof($member) >0) {
+                    $request->merge(array("member" => $member[0]['member_id']));
+                } else {
+                    Log::info($request);
+                    return response()->json(['status' => false, 'error_code' => '202', 'error_msg' => '無效的Token', 'result' => []]);
+                }
             } else {
-                $token = JWTAuth::getToken();
+                Log::info($request);
+                return response()->json(['status' => false, 'error_code' => '202', 'error_msg' => '無效的Token', 'result' => []]);
             }
-            $member = Members::where('api_token','=', $token)->get();
             //$member = JWTAuth::parseToken()->authenticate();
-            $request->merge(array("member" => $member[0]['member_id']));
         } catch (Exception $e) {
             Log::info($e);
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
                 return response()->json(['status' => 'Token is Expired']);
-            }else{
-                return response()->json(['status' => 'Authorization Token not found']);
+            } else {
+                return response()->json(['status' => false, 'error_code' => '202', 'error_msg' => '無效的Token', 'result' => []]);
             }
         }
 
