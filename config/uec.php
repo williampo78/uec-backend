85077cd92155ff8b1dbe1aac715611391cd6e8e4
Rowena <rowena@u-ark.com>
2021-11-11 11:58:35 +0800

    增加config

--- config/uec.php
+++ config/uec.php
@@ -1,10 +1,16 @@
 <?php
 return array(
 
-    'web_category_hierarchy_levels' => '3', 
-    /** 
+    /**
      *   web_category_hierarchy_levels 分類開關
      *   指定為2時，中分類清單不顯示﹝展小類﹞的按鈕
      *   指定為3時，中分類清單顯示﹝展小類﹞的按鈕
+     */
+    'web_category_hierarchy_levels' => '3',
+
+    /*
+     * 預設為測試環境
+     *
     */
+    'isTesting' => env('UEC_TEST', 'true'),
 );
