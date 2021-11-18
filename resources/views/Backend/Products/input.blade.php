00b66c58a073dd0df17d14e828e06408d25269a1
LEO-NB\uark <mopackp47557@gmail.com>
2021-11-18 11:33:55 +0800

    修復類階層內容編輯送出產品後無反應BUG

--- resources/views/Backend/Products/input.blade.php
+++ resources/views/Backend/Products/input.blade.php
@@ -7,54 +7,241 @@
                 <h1 class="page-header"><i class="fa fa-list"></i>分類階層內容編輯</h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">請輸入下列欄位資料</div>
-                    <div class="panel-body" id="category_hierarchy_content_input">
-                        <form role="form" id="new-form" method="POST" action="{{ route('products.store') }}"
-                            enctype="multipart/form-data" novalidate="novalidate">
-                            @csrf
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <div class="col-sm-12">
-                                            <div class="col-sm-3">
-                                                <label for="doc_number">庫存類型 <span class="redtext">*</span></label>
-                                            </div>
-                                            <div class="col-sm-3 form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="active" id="inlineRadio1"
-                                                    value="1">
-                                                <label class="form-check-label" for="inlineRadio1">買斷 [A] </label>
-                                            </div>
-                                            <div class="col-sm-3 form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="active" id="inlineRadio2"
-                                                    value="0">
-                                                <label class="form-check-label" for="inlineRadio2">寄售 [B]</label>
-                                            </div>
-                                            <div class="col-sm-3 form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="active" id="inlineRadio2"
-                                                    value="0">
-                                                <label class="form-check-label" for="inlineRadio2">轉單[T]</label>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="col-sm-3">
-                                        <label for="doc_number">商品序號 <span class="redtext">*</span></label>
-                                    </div>
-                                    <div class="col-sm-9">
-                                        <input class="form-control" name="meta_title" id="meta_title" value="" readonly>
-                                    </div>
-                                </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">請輸入下列欄位資料</div>
+            <div class="panel-body" id="category_hierarchy_content_input">
+                <form class="form-horizontal" role="form" id="new-form" method="POST"
+                    action="{{ route('products.store') }}" enctype="multipart/form-data" novalidate="novalidate">
+                    @csrf
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">庫存類型</label><span class="redtext">*</span>
                             </div>
-                        </form>
+                            <div class="col-sm-3 ">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 買斷 [A]
+                                </label>
+                            </div>
+                            <div class="col-sm-3">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 寄售 [B]
+                                </label>
+                            </div>
+                            <div class="col-sm-3">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> 轉單[T]
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">商品序號</label><span class="redtext">*</span>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="" readonly>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">供應商<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <select class="form-control js-select2" name="active" id="active">
+                                    <option value="">無</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">商品名稱<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">課稅別<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <select class="form-control js-select2" name="active" id="active">
+                                    <option value="">無</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">POS分類<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">品牌<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <select class="form-control js-select2" name="active" id="active">
+                                    <option value="">無</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">商品型號</label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">商品通路<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"> 宅配
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">溫層</label>
+                            </div>
+                            <div class="col-sm-9">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"> 常溫
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">單位<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">最小採購量</label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">效期控管<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-2">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 無
+                                </label>
+                            </div>
+                            <div class="col-sm-3">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 有，天數
+                                </label>
+                            </div>
+                            <div class="col-sm-4">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">允收期(天)</label>
+                            </div>
+                            <div class="col-sm-3">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">商品類型<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-3 ">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 一般品
+                                </label>
+                            </div>
+                            <div class="col-sm-3">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 贈品
+                                </label>
+                            </div>
+                            <div class="col-sm-3">
+                                <label class="radio-inline">
+                                    <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> 加購品
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">重量(公克)<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-3">
+                                <input class="form-control" name="keyword" id="keyword" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">成本(含稅)<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="" readonly>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">毛利(%)<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">採購人員<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="" readonly>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="col-sm-2">
+                                <label class="control-label">轉單審核人員<span class="redtext">*</span></label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="keyword" id="keyword" value="" readonly>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
+
     </div>
 @endsection
 
