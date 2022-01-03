5c30f607b6153b0fbbe72a1b79af9cb95a21a9e7
Rowena <rowena@u-ark.com>
2022-01-03 10:05:55 +0800

    api 購物車增加判斷空陣列

--- app/Services/APICartServices.php
+++ app/Services/APICartServices.php
@@ -524,19 +524,21 @@ class APICartServices
                 }
             }
             $total_amount = ($cartTotal - $cartDiscount);
-            foreach ($campaign_gift['CART'] as $items => $item) {
-                if ($total_amount >= $item->n_value) {
-                    if ($now >= $item->start_launched_at && $now <= $item->end_launched_at) { //在上架期間內
-                        if ($item->campaign_type == 'CART03') { //﹝滿額﹞購物車滿N元，送贈品
-                            if ($item->assignedQty > 0) {
-                                if ($this->stockService->getStockByProd($warehouseCode, $item->product_id)->stock_qty > 0) { //有足夠庫存
-                                    $cartGift[] = array(
-                                        "campaignName" => $item->campaign_name,
-                                        "productId" => $item->product_id,
-                                        "productName" => $item->product_name,
-                                        "productPhoto" => $campaign_gift['PROD'][$item->promotional_campaign_id][$item->product_id]['photo'],
-                                        "assignedQty" => $item->assignedQty
-                                    );
+            if (isset($campaign_gift['CART'])) {
+                foreach ($campaign_gift['CART'] as $items => $item) {
+                    if ($total_amount >= $item->n_value) {
+                        if ($now >= $item->start_launched_at && $now <= $item->end_launched_at) { //在上架期間內
+                            if ($item->campaign_type == 'CART03') { //﹝滿額﹞購物車滿N元，送贈品
+                                if ($item->assignedQty > 0) {
+                                    if ($this->stockService->getStockByProd($warehouseCode, $item->product_id)->stock_qty > 0) { //有足夠庫存
+                                        $cartGift[] = array(
+                                            "campaignName" => $item->campaign_name,
+                                            "productId" => $item->product_id,
+                                            "productName" => $item->product_name,
+                                            "productPhoto" => $campaign_gift['PROD'][$item->promotional_campaign_id][$item->product_id]['photo'],
+                                            "assignedQty" => $item->assignedQty
+                                        );
+                                    }
                                 }
                             }
                         }
