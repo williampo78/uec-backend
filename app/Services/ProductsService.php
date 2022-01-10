4f16b3f3ba0e04a4024d3b769079b35abf0eb700
LEO-NB\uark <mopackp47557@gmail.com>
2022-01-10 12:01:58 +0800

    productsService getProducts function sql 調整

--- app/Services/ProductsService.php
+++ app/Services/ProductsService.php
@@ -91,7 +91,7 @@ class ProductsService
         if (isset($input_data['product_type'])) {
             $products->where('products.product_type', $input_data['product_type']);
         }
-        // dd(Carbon::now()) ; 
+        // dd(Carbon::now()) ;
         //上架狀態
         if (isset($input_data['approval_status'])) {
             switch ($input_data['approval_status']) {
@@ -99,8 +99,7 @@ class ProductsService
                 case 'APPROVED_STATUS_ON':
                     $products = $products->where(function ($query) {
                         $query->where('products.approval_status', '=', 'APPROVED')
-                               ->whereRaw('current_timestamp between products.start_launched_at  and products.end_launched_at');
-                               
+                            ->whereRaw('current_timestamp between products.start_launched_at  and products.end_launched_at');
                         // ->where('products.start_launched_at', '>=', Carbon::now())
                         // ->where('products.end_launched_at', '<=', Carbon::now());
                     });
@@ -108,12 +107,11 @@ class ProductsService
                     break;
                 //商品下架
                 case 'APPROVED_STATUS_OFF':
-                  
+
                     $products = $products->where(function ($query) {
                         $query->where('products.approval_status', '=', 'APPROVED')
-                            
-                              ->where('products.start_launched_at' , '>' ,Carbon::now())
-                              ->orWhere('products.end_launched_at' , '<' ,Carbon::now());
+                            ->where('products.start_launched_at', '>', Carbon::now())
+                            ->orWhere('products.end_launched_at', '<', Carbon::now());
                     });
                     $products = $products->Where(function ($query) {
                         $query->orWhere('products.approval_status', '=', 'CANCELLED');
@@ -133,10 +131,10 @@ class ProductsService
             $products->where('products.start_launched_at', '>=', $start_launched_at);
         }
         //上架結束日
-        if( !empty($input_data['end_launched_at'] )){
+        if (!empty($input_data['end_launched_at'])) {
             $input_data['end_launched_at'] = $input_data['end_launched_at'] . ' 23:59:59';
             $end_launched_at = Carbon::parse($input_data['end_launched_at'])->format('Y-m-d H:i:s');
-            $products->where('products.start_launched_at', '<=', $end_launched_at) ;
+            $products->where('products.start_launched_at', '<=', $end_launched_at);
         };
         // 最低售價
         if (isset($input_data['selling_price_min'])) {
