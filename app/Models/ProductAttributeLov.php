f0d9db4831b620e60876ca7f909ca2f64e1022c4
LEO-NB\uark <mopackp47557@gmail.com>
2022-01-18 15:20:46 +0800

    M008 商品主檔-商城資訊管理 [76]
    階段一應該有﹝認證﹞清單維護，axure文件漏列，請協助增加check box維護
    ----
    check box選單來源：product_attribute_lov
    維護資料table：product_attributes

---
+++ app/Models/ProductAttributeLov.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductAttributeLov extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_attribute_lov';
+    protected $guarded = [];  
+
+}
