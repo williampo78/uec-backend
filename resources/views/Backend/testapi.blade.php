6fbdda54021f2c3e94e7bec25ad8badc616aab7a
LEO-NB\uark <mopackp47557@gmail.com>
2022-02-09 15:05:14 +0800

    Update testapi.blade.php

--- resources/views/Backend/testapi.blade.php
+++ resources/views/Backend/testapi.blade.php
@@ -5,6 +5,7 @@
         <input type="text" name="token" id="token" value="">
         <button id="req">請求</button>
         <button id="login">登入</button>
+        <button id="testheader">測試取得Header</button>
 
     </div>
 
@@ -61,6 +62,32 @@
                     console.log('Authenticated');
 
                     $('#token').val(res.data.result._token) ;
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+                .finally(() => {
+                    /* 不論失敗成功皆會執行 */
+                })
+        });
+
+        $("#testheader").click(function() {
+            // var url = "http://pc.laravel.uec/" ; 
+            var url = "https://uecbackend.u-ark.com/" ; 
+            var token = $('#token').val();
+            const config = {};
+            const bodyParameters = {
+                // mobile: "0999000128",
+                // password:"Taco0224",
+            };
+
+            axios.get(
+                    url + 'api/testheaderauthorization',
+                    bodyParameters,
+                    config
+                ).then((res) => {
+                    console.log(res.headers) ;
+                    console.log(res.headers.authorization);
                 })
                 .catch((error) => {
                     console.error(error)
