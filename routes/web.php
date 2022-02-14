9870cd3490a2fef1882aea31be1e8ce10104bcca
LEO-NB\uark <mopackp47557@gmail.com>
2022-02-14 11:57:31 +0800

    刪除測試api
    刪除測試api

--- routes/web.php
+++ routes/web.php
@@ -212,6 +212,3 @@ Route::group(['prefix' => 'backend', 'middleware' => ['admin']], function () {
 Route::get('/', [LoginAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [LoginAuthController::class, 'customLogin'])->name('login.custom');
 Route::post('custom-registration', [LoginAuthController::class, 'customRegistration'])->name('register.custom');
-Route::get('/axiostest', function () {
-    return view('backend.testapi');
-});
