a1e433a7ffac280103abf9437ce9952855a5f907
LEO-NB\uark <mopackp47557@gmail.com>
2022-03-03 14:23:44 +0800

    Update AdvertisementService.php

--- app/Services/AdvertisementService.php
+++ app/Services/AdvertisementService.php
@@ -835,9 +835,7 @@ class AdvertisementService
         $imageName = $slot->photo_width . 'x' . $slot->photo_height . '_' . uniqid(date('YmdHis')) . $file->getClientOriginalName();
         $path = self::SLOT_CONTENTS_UPLOAD_PATH_PREFIX . $slot_contents_id . '/' . $imageName;
         $img = Image::make($file);
-        $img->resize($slot->photo_width, $slot->photo_height, function ($constraint) {
-            $constraint->aspectRatio();
-        });
+        $img->resize($slot->photo_width, $slot->photo_height);
         $resource = $img->stream()->detach();
         $storagePath = Storage::disk('s3')->put('/'.$path , $resource, 'public');
         if ($storagePath) {
