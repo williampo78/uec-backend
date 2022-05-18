a26e59fc1566038d7f19533e5e3117a4da538789
Rowena <rowena@u-ark.com>
2022-05-18 19:07:20 +0800

    api tappay刷卡金額加上C003折扣

--- app/Services/APIOrderService.php
+++ app/Services/APIOrderService.php
@@ -118,7 +118,7 @@ class APIOrderService
             $webData['cart_campaign_discount'] = $cart_campaign_discount; //原C002滿額折抵
             $webData['cart_p_discount'] = $cart_p_discount;//新C003滿額折抵
             $webData['point_discount'] = $order['point_discount'];
-            $webData['paid_amount'] = ($order['total_price'] + $order['cart_campaign_discount'] + $order['point_discount'] + $order['shipping_fee']);
+            $webData['paid_amount'] = ($order['total_price'] + $order['cart_campaign_discount'] + $order['point_discount'] + $order['shipping_fee'] + $cart_p_discount);
             $webData['points'] = $order['points'];
             $webData['is_paid'] = 0;
             $webData['pay_status'] = 'PENDING';
