c141fa0d6ebea54e541e6682c77fac542f6a6b6c
Rowena <rowena@u-ark.com>
2022-05-19 17:15:56 +0800

    更新TapPay 收款商家

--- config/tappay.php
+++ config/tappay.php
@@ -12,7 +12,7 @@ return [
     'merchant_id' => [
         'TAPPAY_CREDITCARD' => env('TAP_PAY_CREDITCARD'),
         'TAPPAY_LINEPAY' => env('TAP_PAY_LINEPAY'),
-        'TAP_PAY_JKOPAY' => env('TAP_PAY_JKOPAY')
+        'TAPPAY_JKOPAY' => env('TAP_PAY_JKOPAY')
     ],
 
     //銀行對帳系統交易明細
