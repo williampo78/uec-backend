fdde9729c264f78f6ea283bad3290894b969140e
Rowena <rowena@u-ark.com>
2022-06-08 11:13:58 +0800

    調整分類選單以sort方式為key

--- app/Services/APIProductServices.php
+++ app/Services/APIProductServices.php
@@ -54,7 +54,7 @@ class APIProductServices
 
         //根據階層顯示層級資料
         if ($config_levels == '3') {
-            $strSQL = "select cate2.`id` L1ID , cate2.`category_name` L1_NAME, cate1.`id` L2ID , cate1.`category_name` L2_NAME, cate.*, count(cate_prod.`product_id`) as pCount,
+            $strSQL = "select cate2.`sort` L1_SORT, cate2.`id` L1ID , cate2.`category_name` L1_NAME, cate1.`id` L2ID , cate1.`category_name` L2_NAME, cate.*, count(cate_prod.`product_id`) as pCount,
                     '' as campaign_name, '' as url_code, '' as campaign_brief
                     from `web_category_products` cate_prod
                     inner join `web_category_hierarchy` cate on  cate.`id` =cate_prod.`web_category_hierarchy_id`  and cate.`category_level`=3
@@ -77,7 +77,7 @@ class APIProductServices
             $strSQL .= " group by cate.`id`
                     order by cate2.`sort`, cate1.`sort`, cate.`sort`";
         } elseif ($config_levels == '2') {
-            $strSQL = "select cate1.`id` L1ID , cate1.`category_name` L1_NAME, cate.*, count(cate_prod.`product_id`) as pCount,
+            $strSQL = "select cate1.`sort` L1_SORT, cate1.`id` L1ID , cate1.`category_name` L1_NAME, cate.*, count(cate_prod.`product_id`) as pCount,
                     '' as campaign_name, '' as url_code, '' as campaign_brief
                     from `web_category_products` cate_prod
                     inner join `web_category_hierarchy` cate on  cate.`id` =cate_prod.`web_category_hierarchy_id` and cate.`category_level`=2
@@ -98,49 +98,50 @@ class APIProductServices
             $strSQL .= " group by cate.`id`
                     order by cate1.`sort`, cate.`sort`";
         }
+
         $categorys = DB::select($strSQL);
         foreach ($categorys as $category) {
-            $L1_data[$category->L1ID]["id"] = $category->L1ID;
-            $L1_data[$category->L1ID]["name"] = $category->L1_NAME;
+            $L1_data[$category->L1_SORT]["id"] = $category->L1ID;
+            $L1_data[$category->L1_SORT]["name"] = $category->L1_NAME;
 
             if ($config_levels == '3') {
-                $L2_data[$category->L1ID][$category->L2ID]["id"] = $category->L2ID;
-                $L2_data[$category->L1ID][$category->L2ID]["name"] = $category->L2_NAME;
+                $L2_data[$category->L1_SORT][$category->sort]["id"] = $category->L2ID;
+                $L2_data[$category->L1_SORT][$category->sort]["name"] = $category->L2_NAME;
 
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['id'] = $category->id;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['name'] = $category->category_name;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['type'] = $category->content_type;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['meta_title'] = $category->meta_title;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['meta_description'] = $category->meta_description;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['meta_keywords'] = $category->meta_keywords;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['count'] = $category->pCount;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['campaign_name'] = $category->campaign_name;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['url_code'] = $category->url_code;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['campaign_brief'] = $category->campaign_brief;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['sort'] = $category->sort;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['id'] = $category->id;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['name'] = $category->category_name;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['type'] = $category->content_type;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['meta_title'] = $category->meta_title;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['meta_description'] = $category->meta_description;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['meta_keywords'] = $category->meta_keywords;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['count'] = $category->pCount;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['campaign_name'] = $category->campaign_name;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['url_code'] = $category->url_code;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['campaign_brief'] = $category->campaign_brief;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['sort'] = $category->sort;
 
             } else if ($config_levels == '2') {
 
-                $L2_data[$category->L1ID][$category->id]['id'] = $category->id;
-                $L2_data[$category->L1ID][$category->id]['name'] = $category->category_name;
-                $L2_data[$category->L1ID][$category->id]['type'] = $category->content_type;
-                $L2_data[$category->L1ID][$category->id]['meta_title'] = $category->meta_title;
-                $L2_data[$category->L1ID][$category->id]['meta_description'] = $category->meta_description;
-                $L2_data[$category->L1ID][$category->id]['meta_keywords'] = $category->meta_keywords;
-                $L2_data[$category->L1ID][$category->id]['count'] = $category->pCount;
-                $L2_data[$category->L1ID][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
-                $L2_data[$category->L1ID][$category->id]['campaign_name'] = $category->campaign_name;
-                $L2_data[$category->L1ID][$category->id]['url_code'] = $category->url_code;
-                $L2_data[$category->L1ID][$category->id]['campaign_brief'] = $category->campaign_brief;
-                $L2_data[$category->L1ID][$category->id]['sort'] = $category->sort;
+                $L2_data[$category->L1_SORT][$category->id]['id'] = $category->id;
+                $L2_data[$category->L1_SORT][$category->id]['name'] = $category->category_name;
+                $L2_data[$category->L1_SORT][$category->id]['type'] = $category->content_type;
+                $L2_data[$category->L1_SORT][$category->id]['meta_title'] = $category->meta_title;
+                $L2_data[$category->L1_SORT][$category->id]['meta_description'] = $category->meta_description;
+                $L2_data[$category->L1_SORT][$category->id]['meta_keywords'] = $category->meta_keywords;
+                $L2_data[$category->L1_SORT][$category->id]['count'] = $category->pCount;
+                $L2_data[$category->L1_SORT][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
+                $L2_data[$category->L1_SORT][$category->id]['campaign_name'] = $category->campaign_name;
+                $L2_data[$category->L1_SORT][$category->id]['url_code'] = $category->url_code;
+                $L2_data[$category->L1_SORT][$category->id]['campaign_brief'] = $category->campaign_brief;
+                $L2_data[$category->L1_SORT][$category->id]['sort'] = $category->sort;
             }
 
         }
 
         //根據階層顯示層級資料(賣場)
         if ($config_levels == '3') {
-            $strSQL = "select cate2.`id` L1ID , cate2.`category_name` L1_NAME, cate1.`id` L2ID , cate1.`category_name` L2_NAME, cate.*,0 as pCount,
+            $strSQL = "select cate2.`sort` L1_SORT, cate2.`id` L1ID , cate2.`category_name` L1_NAME, cate1.`id` L2ID , cate1.`category_name` L2_NAME, cate.*,0 as pCount,
                     '' as campaign_name, '' as url_code, '' as campaign_brief
                     from `web_category_hierarchy` cate
                     inner join `web_category_hierarchy` cate1 on cate1.`id`=cate.`parent_id`
@@ -159,7 +160,7 @@ class APIProductServices
             $strSQL .= " group by cate.`id`
                     order by cate2.`sort`, cate1.`sort`, cate.`sort`";
         } elseif ($config_levels == '2') {
-            $strSQL = "select cate1.`id` L1ID , cate1.`category_name` L1_NAME, cate.*, 0 as 'pCount',
+            $strSQL = "select cate1.`sort` L1_SORT, cate1.`id` L1ID , cate1.`category_name` L1_NAME, cate.*, 0 as 'pCount',
                     pcc.`campaign_name`, pcc.`url_code`, pcc.`campaign_brief`
                     from web_category_hierarchy cate
                     inner join `web_category_hierarchy` cate1 on cate1.`id`=cate.`parent_id`
@@ -178,39 +179,39 @@ class APIProductServices
         }
         $categorys = DB::select($strSQL);
         foreach ($categorys as $category) {
-            $L1_data[$category->L1ID]["id"] = $category->L1ID;
-            $L1_data[$category->L1ID]["name"] = $category->L1_NAME;
+            $L1_data[$category->L1_SORT]["id"] = $category->L1ID;
+            $L1_data[$category->L1_SORT]["name"] = $category->L1_NAME;
 
             if ($config_levels == '3') {
-                $L2_data[$category->L1ID][$category->L2ID]["id"] = $category->L2ID;
-                $L2_data[$category->L1ID][$category->L2ID]["name"] = $category->L2_NAME;
+                $L2_data[$category->L1_SORT][$category->sort]["id"] = $category->L2ID;
+                $L2_data[$category->L1_SORT][$category->sort]["name"] = $category->L2_NAME;
 
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['id'] = $category->id;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['name'] = $category->category_name;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['type'] = $category->content_type;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['meta_title'] = $category->meta_title;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['meta_description'] = $category->meta_description;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['meta_keywords'] = $category->meta_keywords;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['count'] = $category->pCount;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['campaign_name'] = $category->campaign_name;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['url_code'] = $category->url_code;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['campaign_brief'] = $category->campaign_brief;
-                $L3_data[$category->L1ID][$category->L2ID][$category->id]['sort'] = $category->sort;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['id'] = $category->id;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['name'] = $category->category_name;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['type'] = $category->content_type;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['meta_title'] = $category->meta_title;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['meta_description'] = $category->meta_description;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['meta_keywords'] = $category->meta_keywords;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['count'] = $category->pCount;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['campaign_name'] = $category->campaign_name;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['url_code'] = $category->url_code;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['campaign_brief'] = $category->campaign_brief;
+                $L3_data[$category->L1_SORT][$category->sort][$category->id]['sort'] = $category->sort;
 
             } else if ($config_levels == '2') {
-                $L2_data[$category->L1ID][$category->id]['id'] = $category->id;
-                $L2_data[$category->L1ID][$category->id]['name'] = $category->category_name;
-                $L2_data[$category->L1ID][$category->id]['type'] = $category->content_type;
-                $L2_data[$category->L1ID][$category->id]['meta_title'] = $category->meta_title;
-                $L2_data[$category->L1ID][$category->id]['meta_description'] = $category->meta_description;
-                $L2_data[$category->L1ID][$category->id]['meta_keywords'] = $category->meta_keywords;
-                $L2_data[$category->L1ID][$category->id]['count'] = $category->pCount;
-                $L2_data[$category->L1ID][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
-                $L2_data[$category->L1ID][$category->id]['campaign_name'] = $category->campaign_name;
-                $L2_data[$category->L1ID][$category->id]['url_code'] = $category->url_code;
-                $L2_data[$category->L1ID][$category->id]['campaign_brief'] = $category->campaign_brief;
-                $L2_data[$category->L1ID][$category->id]['sort'] = $category->sort;
+                $L2_data[$category->L1_SORT][$category->id]['id'] = $category->id;
+                $L2_data[$category->L1_SORT][$category->id]['name'] = $category->category_name;
+                $L2_data[$category->L1_SORT][$category->id]['type'] = $category->content_type;
+                $L2_data[$category->L1_SORT][$category->id]['meta_title'] = $category->meta_title;
+                $L2_data[$category->L1_SORT][$category->id]['meta_description'] = $category->meta_description;
+                $L2_data[$category->L1_SORT][$category->id]['meta_keywords'] = $category->meta_keywords;
+                $L2_data[$category->L1_SORT][$category->id]['count'] = $category->pCount;
+                $L2_data[$category->L1_SORT][$category->id]['promotion_campaign_id'] = $category->promotion_campaign_id;
+                $L2_data[$category->L1_SORT][$category->id]['campaign_name'] = $category->campaign_name;
+                $L2_data[$category->L1_SORT][$category->id]['url_code'] = $category->url_code;
+                $L2_data[$category->L1_SORT][$category->id]['campaign_brief'] = $category->campaign_brief;
+                $L2_data[$category->L1_SORT][$category->id]['sort'] = $category->sort;
             }
 
         }
@@ -219,6 +220,7 @@ class APIProductServices
             return 404;
         } else {
             foreach ($L1_data as $key1 => $value1) {
+
                 $data2 = [];
                 $data[$key1]["id"] = $value1["id"];
                 $data[$key1]["name"] = $value1["name"];
