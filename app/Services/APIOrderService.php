e604d8bfac24161ff6e97f68bbe42de17efd429b
Rowena <rowena@u-ark.com>
2022-06-09 18:26:48 +0800

    購物車滿額折抵把最後一筆資料做修正，更新失敗處理

--- app/Services/APIOrderService.php
+++ app/Services/APIOrderService.php
@@ -613,9 +613,9 @@ class APIOrderService
                     }
                 }
             }
-            $pointData = [];
 
             //購物車滿額折抵把最後一筆資料做修正
+            $discountData = [];
             if (($cart_p_discount - $detail_p_discount) != 0) {//購物車的滿額折抵 - 訂單明總的滿額折抵加總 != 0
                 $detail = OrderDetail::where('order_id', '=', $newOrder->id)->where('record_identity', '=', 'M')->orderBy('seq', 'DESC')->first();
 
@@ -628,6 +628,7 @@ class APIOrderService
             }
 
             //點數比例加總不等於1時，把最後一筆資料的比例做修正
+            $pointData = [];
             if ($point_rate != 1 || ($order['points'] - $point_discount) != 0) {
                 $detail = OrderDetail::where('order_id', '=', $newOrder->id)->where('record_identity', '=', 'M')->orderBy('seq', 'DESC')->first();
                 if ($order['points'] > $point_discount) {
