c0c7421bfa79a6bcaff1a2261ca1702f4edf88f2
JayJay <jay.huang@u-ark.com>
2022-06-13 16:18:51 +0800

    trust all proxies

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,11 +16,10 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        /*
         if (!$request->secure() && config('app.enforce_ssl')) {
             return redirect()->secure($request->getRequestUri());
         }
-        */
+
         return $next($request);
     }
 }
