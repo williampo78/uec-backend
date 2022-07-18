35f748d4c97adb86140f2771180332275037c76e
Eric Li <eric.li@u-ark.com>
2022-07-18 17:32:47 +0800

    fix:issue206調整會員訂單，活動的排序

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -536,9 +536,9 @@ class OrderService
             array_multisort($cart['gift'][$key]['campaignProdList'], SORT_ASC);
         }
     }
-    foreach ($order_details as $key => $val) {
-        array_multisort($order_details[$key]['discount_content'], SORT_ASC);
-    }
+//    foreach ($order_details as $key => $val) {
+//        array_multisort($order_details[$key]['discount_content'], SORT_ASC);
+//    }
     $orders['results']['thresholdAmount'] = config('uec.cart_p_discount_split') == 1 ? 0 : $thresholdAmount;
     $orders['results']['order_details'] = $order_details;
     //是否顯示thresholdDiscount
