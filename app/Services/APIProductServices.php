0ddbc433188aa5669621e038ef5584f502144505
Rowena <rowena@u-ark.com>
2022-09-29 16:21:26 +0800

    [#sit-issue-360前台【購物車step2】分期付款相關問題、訂單摘要結帳商品數異常] (2) 可分期銀行的排序請按照銀行代碼(bank_no)排序

--- app/Services/APIProductServices.php
+++ app/Services/APIProductServices.php
@@ -1138,9 +1138,9 @@ class APIProductServices
             ->when(isset($min_consumption), function ($query) use ($min_consumption) {
                 $query->where('min_consumption', '<=', $min_consumption);
             })
-            ->orderBy('issuing_bank_no', 'asc')
             ->orderBy('interest_rate', 'asc')
             ->orderBy('number_of_installments', 'asc')
+            ->orderBy('issuing_bank_no', 'asc')
             ->get([
                 'id',
                 'issuing_bank_no',
