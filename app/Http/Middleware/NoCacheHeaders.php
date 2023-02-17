e351ac70172e3620fb1758903717f3a03907a45d
LEON-NB\Leo <leo.cai@u-ark.com>
2023-02-17 10:12:13 +0800

    #pre-test-903 後台、供應商【共用】用戶的帳號登出後，點瀏灠器的上一頁，請將頁面導至「登入頁」《pre#sup-33》

--- app/Http/Middleware/NoCacheHeaders.php
+++ app/Http/Middleware/NoCacheHeaders.php
@@ -17,10 +17,15 @@ class NoCacheHeaders
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-
-        $response->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
-        $response->header('Cache-Control', 'no-cache, must-revalidate, no-store, max-age=0, private');
-
+        $headers = [
+            'Cache-Control' => 'nocache, no-store, max-age=0, must-revalidate',
+            'Pragma', 'no-cache',
+            'Expires', 'Fri, 01 Jan 1990 00:00:00 GMT',
+        ];
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+        
         return $response;
     }
 }
